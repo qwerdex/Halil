3b9f4844e86c509eaa4904ed1266300c0b495224
qwerdex <6620541@gmail.com>
2023-12-28 20:07:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     var texts = [
-        "Art is never finished, only abandoned", 
-        "Creativity is allowing yourself to make mistakes. Art is knowing which ones to keep.",
-        "Art does not reproduce the visible; rather, it makes visible",
-        // Add more quotes as desired
+        // Добавьте здесь свои цитаты
     ];
 
-    var randomIndex = Math.floor(Math.random() * texts.length);
-    var randomText = texts[randomIndex];
-    var textContainer = document.getElementById('randomText');
-    textContainer.innerText = randomText;
+    function updateText() {
+        var randomIndex = Math.floor(Math.random() * texts.length);
+        var randomText = texts[randomIndex];
+        document.getElementById('randomText').innerText = randomText;
+    }
 
-    document.getElementById('shareBtn').addEventListener('click', function() {
-        html2canvas(textContainer).then(canvas => {
-            var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    updateText();
+
+    document.getElementById('curateBtn').addEventListener('click', function() {
+        updateText();
+    });
+
+    document.getElementById('downloadBtn').addEventListener('click', function() {
+        html2canvas(document.getElementById('randomText')).then(canvas => {
             var link = document.createElement('a');
             link.download = 'curated-text.png';
-            link.href = image;
+            link.href = canvas.toDataURL();
             link.click();
         });
     });
